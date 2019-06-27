f4d2f6f858fe32705246fe51d448fabdb5999408
imrenagi <imre.nagi2812@gmail.com>
2019-06-27 22:51:12 +0700

    Add dockerfile

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,5 +52,9 @@ module.exports = {
      ** You can extend webpack config here
      */
     extend(config, ctx) {}
-  }
+  },
+  server: {
+    port: 8080, // default: 3000
+    host: '0.0.0.0', // default: localhost
+  },
 }
