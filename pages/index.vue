b04156a274d3ac0dead574e5d7a4654832a99f7d
Imre Nagi <imre.nagi2812@gmail.com>
2019-07-13 10:46:31 +0700

    lampung

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,10 @@
     <div>
       <logo />
       <h1 class="title">
-        frontend
+        LAMPUNG
       </h1>
       <h2 class="subtitle">
-        demo next extended
+        GDG X TEKNOKRAT
       </h2>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">
